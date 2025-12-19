1b2d3f923588d041121545dcefa24ad8badf9643
Shubham Ranjan <gkr74226@gmail.com>
2025-12-19 11:55:01 +0530

    Commit...

--- src/main/java/in/sr/voteasy/controllers/CandidateController.java
+++ src/main/java/in/sr/voteasy/controllers/CandidateController.java
@@ -1,5 +1,36 @@
 package in.sr.voteasy.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import in.sr.voteasy.entities.Candidate;
+import in.sr.voteasy.services.CandidateService;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/candidate")
+@CrossOrigin
 public class CandidateController {
+	private CandidateService candidateService;
+
+	@Autowired
+	public CandidateController(CandidateService candidateService) {
+		this.candidateService = candidateService;
+	}
+	
+	@PostMapping("/add")
+	public ResponseEntity<Candidate> addCandidate(@RequestBody @Valid Candidate candidate){
+		Candidate savedCandidate=candidateService.addCandidate(candidate);
+		return new ResponseEntity<Candidate>(savedCandidate,HttpStatus.CREATED);
+	}
+	
+	
+	
 
 }
