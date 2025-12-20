aa788f2258f86eceb18c33f8a1779e19b3a4c7c8
Shubham Ranjan <gkr74226@gmail.com>
2025-12-20 15:15:23 +0530

    Commit..

--- src/main/java/in/sr/voteasy/controllers/ElectionResultController.java
+++ src/main/java/in/sr/voteasy/controllers/ElectionResultController.java
@@ -1,8 +1,11 @@
 package in.sr.voteasy.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,6 +38,11 @@ public class ElectionResultController {
 		responseDTO.setWinnerVotes(result.getWinner().getVoteCount());
 		
 		return ResponseEntity.ok(responseDTO);
+	}
+	@GetMapping
+	public ResponseEntity<List<ElectionResult>> getAllResults(){
+		List<ElectionResult> results= electionResultService.getAllResults();
+		return ResponseEntity.ok(results);
 	}
 
 }
